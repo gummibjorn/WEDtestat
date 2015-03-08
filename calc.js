2d87b5ae98dca42a9fb37e5aef9c7d1c5d2987b4
gummibjorn <marioegli95@gmail.com>
2015-03-08 13:48:58 +0100

    EventListener added for Numbers

--- calc.js
+++ calc.js
@@ -1,8 +1,16 @@
 function add(a){
-	console.log(a);
+    console.log(a);	
 }
 
+function init(){
+  document.getElementsByClassName("numberFields")[0].children[1].addEventListener("click", function() { add(2); }, false);
+  document.getElementsByClassName("numberFields")[0].children[2].addEventListener("click", function() { add(3); }, false);
+  document.getElementsByClassName("numberFields")[0].children[3].addEventListener("click", function() { add(4); }, false);
+  document.getElementsByClassName("numberFields")[0].children[4].addEventListener("click", function() { add(5); }, false);
+  document.getElementsByClassName("numberFields")[0].children[5].addEventListener("click", function() { add(6); }, false);
+  document.getElementsByClassName("numberFields")[0].children[6].addEventListener("click", function() { add(7); }, false);
+  document.getElementsByClassName("numberFields")[0].children[7].addEventListener("click", function() { add(8); }, false);
+  document.getElementsByClassName("numberFields")[0].children[8].addEventListener("click", function() { add(9); }, false);
+}
 
-window.onload = function(){
-document.getElementsByClassName("numberFields")[0].children[1].addEventListener("click", add(2), false);
-};
+window.addEventListener('load', init); 
