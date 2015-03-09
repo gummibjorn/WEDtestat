cec2e2d20a032c7642f8d7b9e948e8727cdf6720
gummibjorn <marioegli95@gmail.com>
2015-03-09 17:28:56 +0100

    Eventhandler added

--- calc.js
+++ calc.js
@@ -1,16 +1,12 @@
-function add(a){
-    console.log(a);	
+function add(event){
+    console.log(event.target.value || event.srcElement.value);	
 }
 
 function init(){
-  document.getElementsByClassName("numberFields")[0].children[1].addEventListener("click", function() { add(2); }, false);
-  document.getElementsByClassName("numberFields")[0].children[2].addEventListener("click", function() { add(3); }, false);
-  document.getElementsByClassName("numberFields")[0].children[3].addEventListener("click", function() { add(4); }, false);
-  document.getElementsByClassName("numberFields")[0].children[4].addEventListener("click", function() { add(5); }, false);
-  document.getElementsByClassName("numberFields")[0].children[5].addEventListener("click", function() { add(6); }, false);
-  document.getElementsByClassName("numberFields")[0].children[6].addEventListener("click", function() { add(7); }, false);
-  document.getElementsByClassName("numberFields")[0].children[7].addEventListener("click", function() { add(8); }, false);
-  document.getElementsByClassName("numberFields")[0].children[8].addEventListener("click", function() { add(9); }, false);
+  var elements = document.getElementsByTagName("input");
+  for(index = 2; index < elements.length; index++){
+    elements[index].addEventListener("click", function() { add(event); }, false); 
+  }	
 }
 
 window.addEventListener('load', init); 
