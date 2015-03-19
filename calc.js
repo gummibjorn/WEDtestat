664b30c3de96d54c9e41928646afde16a6b166ba
tourn <latzer.daniel@gmail.com>
2015-03-19 08:50:14 +0100

    jQuerify code

--- calc.js
+++ calc.js
@@ -2,58 +2,55 @@
 var a, b, op, result;
 
 function inputDigit(event){
-    var digit = event.target.value;
-    var display = document.getElementsByTagName("input")[0];
-    if(op === null){
-      if(a === ""){
-        display.value = "";
-      }
-      a += digit;
-    } else {
-      b += digit;
+  var digit = event.target.value;
+  var display = jQuery(".header input");
+  if(op === null){
+    if(a === ""){
+      //clear the 0 from start state
+      display.val("");
     }
-    display.value = display.value + digit;
+    a += digit;
+  } else {
+    b += digit;
+  }
+  display.val(display.val() + digit);
 }
 
 function sum(){
   result = op(parseInt(a),parseInt(b));
   reset();
-  document.getElementsByTagName("input")[0].value = result;
+  jQuery(".header input").val(result);
 }
 
 function reset(){
 	a="";
 	b="";
 	op=null;
-  var display = document.getElementsByTagName("input")[0];
-  display.value = "0";
+  jQuery(".header input").val(0);
 }
 
 function init(){
-  var numbers = document.getElementsByClassName("numberFields")[0].getElementsByTagName("input");
-  for(var i = 0; i < numbers.length; i++){
-    numbers[i].addEventListener("click", function() { inputDigit(event); }, false);
-  }
+  jQuery(".numberFields").click(function(event){
+    inputDigit(event);
+  });
 
-  var operations = document.getElementsByClassName("operations")[0].getElementsByTagName("input");
-  operations[0].addEventListener("click", function() { op = function(a,b) { return a+b }});
-  operations[1].addEventListener("click", function() { op = function(a,b) { return a-b }});
-  operations[2].addEventListener("click", function() { op = function(a,b) { return a*b }});
-  operations[3].addEventListener("click", function() { op = function(a,b) { return a/b }});
-  for(var i = 0; i < operations.length; i++){
-    operations[i].addEventListener("click", function(event) {
-      //weiterrechnen
-      var display = document.getElementsByTagName("input")[0];
-      display.value = display.value + event.target.value;
-      if(result){
-        a = result;
-        b = "";
-      }
+  jQuery('.operations').children()
+    .first().click(function(){ op = function(a,b) { return a+b; }})
+    .next().click(function() { op = function(a,b) { return a-b; }})
+    .next().click(function() { op = function(a,b) { return a*b; }})
+    .next().click(function() { op = function(a,b) { return a/b; }});
 
-    });
-  }
-  document.getElementsByClassName("sum")[0].getElementsByTagName("input")[0].addEventListener("click", function() { sum(); });
+  jQuery('.operations').click(function(event){
+    //weiterrechnen
+    var display = jQuery(".header input");
+    display.val(display.val() + event.target.value);
+    if(result){
+      a = result;
+      b = "";
+    }
+  });
 
+  jQuery('.sum').click(sum);
   reset();
 }
 
